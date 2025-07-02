4c5eb60041c9cc632ab52cac36c45744d16fc2d2
Sajid Kazmi <uptgym@gmail.com>
2025-07-02 21:08:03 +0100

    adress loading and payprop start

--- src/main/java/site/easy/to/build/crm/entity/AccountType.java
+++ src/main/java/site/easy/to/build/crm/entity/AccountType.java
@@ -1,14 +1,14 @@
 package site.easy.to.build.crm.entity;
 
 public enum AccountType {
-    individual,  
-    business;    
+    INDIVIDUAL,
+    BUSINESS;
     
     public String getValue() {
-        return this.name();  // Returns "individual" or "business"
+        return this.name().toLowerCase();  // Returns "individual" or "business" for PayProp
     }
     
     public String getDisplayName() {
-        return this.name().substring(0, 1).toUpperCase() + this.name().substring(1);
+        return this.name().substring(0, 1).toUpperCase() + this.name().substring(1).toLowerCase();
     }
 }
