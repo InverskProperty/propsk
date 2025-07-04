8038b8ee9d622feeabdaf6842552558a3d413eb4
Sajid Kazmi <uptgym@gmail.com>
2025-07-04 19:55:33 +0100

    changed property entity for debug

--- src/main/java/site/easy/to/build/crm/entity/Customer.java
+++ src/main/java/site/easy/to/build/crm/entity/Customer.java
@@ -294,7 +294,6 @@ public class Customer {
     public void setCustomerId(Integer customerId) { this.customerId = customerId; }
 
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
