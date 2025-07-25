910c3bad59cdb1775399fb726160d2bbc8fe0827
Sajid Kazmi <uptgym@gmail.com>
2025-07-25 18:18:19 +0100

    database fixes and ticket controller issues debug

--- src/main/java/site/easy/to/build/crm/repository/BatchPaymentRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/BatchPaymentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.easy.to.build.crm.entity.BatchPayment;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
