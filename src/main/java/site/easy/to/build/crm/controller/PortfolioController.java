6edf070cbe1c0bd0995d259a664b86ab22935144
Sajid Kazmi <uptgym@gmail.com>
2025-08-10 21:16:26 +0100

    Security fix user id

--- src/main/java/site/easy/to/build/crm/controller/PortfolioController.java
+++ src/main/java/site/easy/to/build/crm/controller/PortfolioController.java
@@ -23,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.Optional;
 
 import site.easy.to.build.crm.controller.PortfolioController.PortfolioWithAnalytics;
 import site.easy.to.build.crm.entity.*;
