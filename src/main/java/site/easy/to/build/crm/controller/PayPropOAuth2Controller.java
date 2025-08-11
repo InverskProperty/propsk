acb390af8afc4c864e30f71e2be0d0f3341e2616
Sajid Kazmi <uptgym@gmail.com>
2025-08-11 12:45:35 +0100

    Security fix user id

--- src/main/java/site/easy/to/build/crm/controller/PayPropOAuth2Controller.java
+++ src/main/java/site/easy/to/build/crm/controller/PayPropOAuth2Controller.java
@@ -38,6 +38,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import site.easy.to.build.crm.util.AuthenticationUtils;
+import org.springframework.http.MediaType;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
