ac4c63492142f2f549e4b360d6cf08821729b7af
Sajid Kazmi <uptgym@gmail.com>
2025-08-15 02:23:21 +0100

    Security fix user id

--- src/main/java/site/easy/to/build/crm/controller/PortfolioController.java
+++ src/main/java/site/easy/to/build/crm/controller/PortfolioController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashMap;
 import java.util.Map;
